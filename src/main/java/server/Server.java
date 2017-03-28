0705dece455908ff160b7781d3aa45dcbb2df999
Denys Petrenko <quzya11@gmail.com>
2017-03-28 15:34:38 +0300

    New features: Multiclient support; Port definition in program args;  Command exit in Client;

--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -1,11 +1,6 @@
 package server;
 
-import protocol.CustomRequest;
-import protocol.CustomResponse;
-
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -13,39 +8,21 @@ public class Server {
 
 
     public static void main(String[] args) throws IOException {
-        try(
-                ServerSocket socket = new ServerSocket(4444);
-                Socket clientSocket = socket.accept();
-                ObjectOutputStream outputStream = new ObjectOutputStream(clientSocket.getOutputStream());
-                ObjectInputStream inputStream = new ObjectInputStream(clientSocket.getInputStream())){
 
+        if (args.length != 1) {
+            System.err.println("Usage: java server <port number>");
+            System.exit(1);
+        }
+        try(ServerSocket socket = new ServerSocket(Integer.parseInt(args[0]))) {
             while (true) {
-                CustomRequest request = (CustomRequest) inputStream.readObject();
-                CustomResponse response = new CustomResponse(new Controller().execute(request));
-                outputStream.writeObject(response);
+                Socket clientSocket = socket.accept();
+                Thread thread = new Thread(new ClientThread(clientSocket));
+                thread.start();
             }
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
         }
     }
 
-    private static class Controller{
 
-        Object execute(CustomRequest request) throws IOException {
-            switch (request.getCommand()) {
-                case "list":return Dao.getAll();
-                case "info_depositor": return Dao.getDepositsByDepositor(request.getDeposit().getDepositor());
-                case "info_bank":return Dao.getDepositsByBank(request.getDeposit().getBankName());
-                case "info_id": return Dao.getDepositById(request.getDeposit().getAccountID());
-                case "info_type":return Dao.getDepositsByType(request.getDeposit().getType());
-                case "insert":return Dao.insert(request.getDeposit());
-                case "delete":return Dao.delete(request.getDeposit().getAccountID());
-
-            }
-            return null;
-        }
-
-    }
 
 
 }
